1712073b8a6acfd33afff106206909f468ec7686
suresh <sureshgairola23@gmail.com>
2022-10-11 08:08:03 +0530

    working on forgot password

--- src/app/index/forgot-password/forgot-password.component.ts
+++ src/app/index/forgot-password/forgot-password.component.ts
@@ -1,15 +1,66 @@
 import { Component, OnInit } from '@angular/core';
-
+import {AdminService  } from "../../services/admin.service";
+import { Router } from "@angular/router";
+import { ApiService } from "../../services/api.service";
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.css']
 })
 export class ForgotPasswordComponent implements OnInit {
+  refreshLink = 0;
+  showform = 'new';
+  // Object for forget Password
+  forgetpasswordObj = {
+    email: '',
+    otp: '',
+    password: '',
+    cpassword: '',
+    _csrf: null,
+  };
+  // error object
+  errorObj = {
+    email: '',
+    otp: '',
+    password: '',
+    cpassword: '',
 
-  constructor() { }
+  };
+  constructor(private adminService: AdminService,
+    private router: Router,
+    private apiService: ApiService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    if (this.apiService.isLoggedIn()) {
+      this.router.navigateByUrl('/dashboard');
+
+    } else {
+      //this.getCsrf();
+    }
   }
+
+  // Function to remove validation error
+  clearError(field) {
+    switch (field) {
+      case 'otp':
+        this.errorObj.otp = '';
+        break;
+      case 'email':
+        this.errorObj.email = '';
+        break;
+      case 'password':
+        this.errorObj.password = '';
+        break;
+      case 'cpassword':
+        this.errorObj.cpassword = '';
+        break;
+
+
+    }
+  }
+  // forget password work start
+
+  // get otp by email
+
 
 }
